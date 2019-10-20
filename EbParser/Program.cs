a4fcb2b8329dda68559335f3de5988eeeb7cc9e4
Dr1N <drn.exp@gmail.com>
2019-10-20 21:59:30 +0300

    Add project files.

---
+++ EbParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EbParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
