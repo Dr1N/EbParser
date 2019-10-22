c1d3cd2ed1f940728e9ae7ca1739ab40749fb333
Dr1N <drn.exp@gmail.com>
2019-10-22 03:32:09 +0300

    stubs

--- EbParser/Program.cs
+++ EbParser/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace EbParser
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stopWatch = Stopwatch.StartNew();
+           
+            var worker = new Parser();
+            worker.PageChangded += Worker_PageChangded;
+            worker.Error += Worker_Error;
+            await worker.ParseAsync();
+
+            Console.WriteLine($"Time: {stopWatch.Elapsed.TotalSeconds} sec");
+        }
+
+        private static void Worker_Error(object sender, string e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Worker_PageChangded(object sender, Uri e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
